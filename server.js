7db61a6b4825e232e562f2ee7a77d4b058d110ce
unknown <shirleyhaddix82@outlook.com>
2025-06-11 22:34:01 -0400

    Adjusted entry point in express

--- server.js
+++ server.js
@@ -3,12 +3,12 @@
 const express = require("express");
 const app = express();
 const port = 8080;
-app.use(express("/public"));
+app.use(express.static("./public"));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 app.listen(port, () => {
-    console.log(`Server is running http://localhost: ${port}`);
+    console.log(`Server is running http://localhost:${port}`);
     console.log("Press Ctrl+C to end this process.");
 });
 
