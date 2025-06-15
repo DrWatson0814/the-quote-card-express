4f6862ddaa53b344f8323ca50284afe4b46f889d
unknown <shirleyhaddix82@outlook.com>
2025-06-15 18:53:48 -0400

    Finishing touches

--- public/js/index.js
+++ public/js/index.js
@@ -9,12 +9,12 @@ const quoteElement = document.getElementById("quote");
 const authorElement = document.getElementById("author");
 
 async function getRandomImage() {
-  const client_id = '';
-  const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
+  const endpoint = `http://localhost:8080/api/v1/getRandomImage`;
   try {
     const response = await fetch(endpoint);
     const returnedData = await response.json();
-    const receivedPhotoUrl = returnedData.urls.regular;
+    const receivedPhotoUrl = returnedData.data;
+    
     const randomPic = document.querySelector(".background-img");
     randomPic.style.backgroundImage = `url("${receivedPhotoUrl}")`;
   } catch (error) {
